1a45ead33d9c8529e732e94b66606f898916840a
noah <tsos201@outlook.com>
2024-07-11 11:15:27 +0800

    1. record dll information via json

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,10 @@
-use crate::entity::function::{Function, FunctionTypes};
 
 mod handler;
 mod tools;
 pub mod entity;
 
 #[no_mangle]
-pub extern "C" fn add(left: usize, right: usize) -> usize {
+pub extern "C" fn test(left: usize, right: usize) -> usize {
+    println!("aaabbbccc");
     left + right
-}
-
-#[no_mangle]
-pub extern "C" fn list() -> Vec<Function> {
-    return vec![
-        Function{
-            name: "test".to_string(),
-            description: "sss".to_string(),
-            params: vec![],
-            return_type: FunctionTypes::Str,
-            is_async: false,
-            is_static: false,
-        }
-    ];
 }
